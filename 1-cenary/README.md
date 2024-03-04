686193b94765b8053156220caba6bd085b3aef89
holynacll <alexandrecurylima@hotmail.com>
2024-03-04 00:11:17 -0300

    fix bug traci tl

--- 1-cenary/README.md
+++ 1-cenary/README.md
@@ -1,5 +1,6 @@
 Cenário: 
-  - network grid, com carros, semáforos e *hospitais*.
+  - network grid, com carros, semáforos e Ponto A e Ponto B
+    - distancia eucl, manh e harvesine
   - ocorrência de acidente (parada de carro/congestionamento no local/fechamento da via)
     - dispara ambulância até o local e vai até o hospital mais próximo
   - criação do algoritmo de liberação de sinal verde
@@ -9,9 +10,9 @@ Cenário:
 
 0 - $SUMO_HOME = "C:\Users\Alexandre Cury\miniconda3\envs\sumo-env\Lib\site-packages\sumo"
 
-1 - netgenerate --grid --grid.number=4 --grid.length=75 -j traffic_light --output-file=cenary.net.xml
+1 - netgenerate --grid --grid.number=4 --grid.length=150 --default-junction-type traffic_light --output-file=cenary.net.xml 
 
-2 - python $SUMO_HOME/tools/randomTrips.py -n cenary.net.xml -o odtrips.car.xml -r cenary.car.rou.xml --prefix car --period 1 -p 2 --trip-attributes='color=\"0,0,1\"'
+2 - python $SUMO_HOME/tools/randomTrips.py -n cenary.net.xml -o odtrips.car.xml -r cenary.car.rou.xml --prefix car --period 1 -p 2 --trip-attributes='color=\"0,0,1\" accel=\"0.8\" decel=\"4.5\" sigma=\"0.5\" length=\"5\" minGap=\"2.5\" maxSpeed=\"16.67\" guiShape=\"passenger\"'
 
 3 - python $SUMO_HOME/tools/randomTrips.py -n cenary.net.xml -o odtrips.emergency.xml -r cenary.emergency.rou.xml --prefix emergency --period 100 -p 10 --vehicle-class emergency --trip-attributes='color=\"1,0,0\"'
 
@@ -21,8 +22,19 @@ last - sumo-gui cenary.sumocfg
 
 
 bibliografia:
-Analysis and Modelling of Road Traffic Using SUMO to Optimize the Arrival Time of Emergency Vehicles - https://www.youtube.com/watch?v=GlPf7TmuI9E
+Analysis and Modelling of Road Traffic Using SUMO to Optimize the Arrival Time of Emergency Vehicles - https://www.tib-op.org/ojs/index.php/scp/article/view/225/428 - https://www.youtube.com/watch?v=GlPf7TmuI9E (verificar o algoritmo)
 Intelligent traffic management for emergency vehicles with a simulation case study - https://www.youtube.com/watch?v=7rpXvYsNFIE
 A Programmer's Note on TraCI_tls, TraCI, and SUMO - https://intelaligent.github.io/tctb/post-learning-traci-tls.html
+Quantifying the impact of connected and autonomous vehicles on traffic efficiency and safety in mixed traffic
 
+refs:
+Lane-Changing Model in SUMO
+https://arxiv.org/pdf/2304.05982.pdf
+https://cst.fee.unicamp.br/sites/default/files/sumo/sumo-roadmap.pdf
 https://github.com/eclipse-sumo/sumo/issues/4312
+https://www.researchgate.net/publication/37454736_TraCI_An_Interface_for_Coupling_Road_Traffic_and_Network_Simulators?enrichId=rgreq-481d19cd502f6889985e24af3c6715e4-XXX&enrichSource=Y292ZXJQYWdlOzM3NDU0NzM2O0FTOjk5NzQ2NDUwNTA5ODMyQDE0MDA3OTI4MTY1NTY%3D&el=1_x_3&_esc=publicationCoverPdf
+
+
+
+bugs:
+cars not stopping on red light: https://sourceforge.net/p/sumo/mailman/message/30703209/
--- 1-cenary/main.py
+++ 1-cenary/main.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import time
+import optparse
 os.environ['SUMO_HOME'] = "C:\\Users\\Alexandre Cury\\miniconda3\\envs\\sumo-env\\Lib\\site-packages\\sumo"
 
 if 'SUMO_HOME' in os.environ:
@@ -9,13 +10,26 @@ if 'SUMO_HOME' in os.environ:
 else:
     sys.exit("please declare environment variable 'SUMO_HOME'")
 
-sumoBinary = os.path.join(tools, "sumo-gui")
-sumo_gui = 'sumo-gui'
-file = "cenary.sumocfg"
-sumoCmd = [sumo_gui, "-c", file]
+# sumoBinary = os.path.join(tools, "sumo-gui")
+# sumo_gui = 'sumo-gui'
+# file = "cenary.sumocfg"
+# sumoCmd = [sumo_gui, "-c", file]
 
 import traci
-traci.start(sumoCmd)
+from sumolib import checkBinary  # noqa
+# traci.start(sumoCmd)
+
+def check_tls(tls_id):
+    print("checking tls_id: ", tls_id)
+    print("ryg", traci.trafficlight.getRedYellowGreenState(tls_id))
+    print("rygdef", traci.trafficlight.getAllProgramLogics(tls_id))
+    print("lanes", traci.trafficlight.getControlledLanes(tls_id))
+    print("links", traci.trafficlight.getControlledLinks(tls_id))
+    print("program", traci.trafficlight.getProgram(tls_id))
+    print("phase", traci.trafficlight.getPhase(tls_id))
+    print("phaseName", traci.trafficlight.getPhaseName(tls_id))
+    print("switch", traci.trafficlight.getNextSwitch(tls_id))
+
 
 def shouldContinueSim():
     """Checks that the simulation should continue running.
@@ -25,52 +39,102 @@ def shouldContinueSim():
     numVehicles = traci.simulation.getMinExpectedNumber()
     return True if numVehicles > 0 else False
 
-step = 1
-while shouldContinueSim():
-    print(step)
-    time.sleep(0.5)
-    traci.simulationStep()
-    vehicles = traci.vehicle.getIDList()
-    for vehicle_id in vehicles:
-        type_id = traci.vehicle.getTypeID(vehicle_id)
-        classname = traci.vehicle.getVehicleClass(type_id)
-        if classname == 'emergency':
-            print(classname)
-            next_tls = traci.vehicle.getNextTLS(vehicle_id)
-            print("next tls for vehicle emergency:", vehicle_id, " is ", next_tls)
-    # tls_id_list = traci.trafficlight.getIDList()
-    # for tls_id in tls_id_list:
-    #     links_controlleds = traci.trafficlight.getControlledLinks(tls_id)
-    #     print('links controllers: ', links_controlleds)
-    #     for links_set_list in links_controlleds:
-    #         for link_set in links_set_list:
-    #             for link in link_set:
-    #                 print('link: ', link)
-    #                 vehiclesblock = traci.trafficlight.getBlockingVehicles(tls_id, str(link))
-    #                 print('cars blocked', vehiclesblock)
-    # print(edges)
-    # for edge_id in edges:
-    #     if traci.inductionloop.getLastStepVehicleNumber(edge_id) > 0:
-    #         print(f"carro passou pela edge {edge_id}")
-    
-    if step%10==0:
-        for i in range(0, len(vehicles)):
-           print(vehicles[i])
-           traci.vehicle.setSpeedMode(vehicles[i], 0)
-           traci.vehicle.setSpeed(vehicles[i], 15)  
-           traci.gui.toggleSelection(vehicles[i])
-           
-           
-           print("Speed ", vehicles[i], ": ", traci.vehicle.getSpeed(vehicles[i]), " m/s")
-           print("CO2Emission ", vehicles[i], ": ", traci.vehicle.getCO2Emission(vehicles[i]), " mg/s")
-           print("EdgeID of veh ", vehicles[i], ": ", traci.vehicle.getRoadID(vehicles[i]))
-           print("Distance ", vehicles[i], ": ", traci.vehicle.getDistance(vehicles[i]), " m")
-    step += 1
+def run():
+    step = 0
+    while shouldContinueSim():
+        # time.sleep(0.5)
+        traci.simulationStep()
+        vehicles = traci.vehicle.getIDList()
+        # for vehicle_id in vehicles:
+        #     type_id = traci.vehicle.getTypeID(vehicle_id)
+        #     if type_id == 'emergency_emergency':
+        #         print(type_id)
+        #         """
+        #         Return list of upcoming traffic lights [(tlsID, tlsIndex, distance, state), ...]
+        #         cada tls está em ordem crescente de distância 
+        #         The lanePosition is the driving distance from the start of the edge (road) in meters
+        #         """
+        #         next_tls_set = traci.vehicle.getNextTLS(vehicle_id)
+        #         for tls in next_tls_set:
+        #             # get tls
+        #             tls_id = tls[0]
+        #             tls_index = tls[1]
+        #             vehicle_distance_to_tls = tls[2]
+        #             tls_state = tls[3]
+        #             check_tls(tls_id)
+                    # tls_phases = traci.trafficlight.getRedYellowGreenState(tls_id)
+                    # print(tls_phases)
+                    # for index, phase in enumerate(tls_phases):
+                    #     if phase in ('g', 'G'):
+                    #         traci.trafficlight.setPhase(tls_id, int(index))
+                    #         traci.trafficlight.setPhaseDuration(tls_id, 42)
+                    #         break
+                    
+                    # if
+                    # vehicle_speed = traci.vehicle.getSpeed(vehicle_id)
+                    # time_to_tls = tls_distance/vehicle_speed
+                # print("next tls for vehicle emergency:", vehicle_id, " is ", next_tls_set)
+        # tls_id_list = traci.trafficlight.getIDList()
+        # for tls_id in tls_id_list:
+        #     links_controlleds = traci.trafficlight.getControlledLinks(tls_id)
+        #     print('links controllers: ', links_controlleds)
+        #     for links_set_list in links_controlleds:
+        #         for link_set in links_set_list:
+        #             for link in link_set:
+        #                 print('link: ', link)
+        #                 vehiclesblock = traci.trafficlight.getBlockingVehicles(tls_id, str(link))
+        #                 print('cars blocked', vehiclesblock)
+        # print(edges)
+        # for edge_id in edges:
+        #     if traci.inductionloop.getLastStepVehicleNumber(edge_id) > 0:
+        #         print(f"carro passou pela edge {edge_id}")
+        
+        if step%10==0:
+            for i in range(0, len(vehicles)):
+                print(vehicles[i])
+                traci.vehicle.setSpeed(vehicles[i], 15)  
+                traci.gui.toggleSelection(vehicles[i])
+                
+                
+        #     print("Speed ", vehicles[i], ": ", traci.vehicle.getSpeed(vehicles[i]), " m/s")
+        #     print("CO2Emission ", vehicles[i], ": ", traci.vehicle.getCO2Emission(vehicles[i]), " mg/s")
+        #     print("EdgeID of veh ", vehicles[i], ": ", traci.vehicle.getRoadID(vehicles[i]))
+        #     print("Distance ", vehicles[i], ": ", traci.vehicle.getDistance(vehicles[i]), " m")
+        step += 1
 
-#get network parameters
-IDsOfEdges=traci.edge.getIDList()
-print("IDs of the edges:", IDsOfEdges)
-IDsOfJunctions=traci.junction.getIDList()
-print("IDs of junctions:", IDsOfJunctions)
+    #get network parameters
+    IDsOfEdges=traci.edge.getIDList()
+    print("IDs of the edges:", IDsOfEdges)
+    IDsOfJunctions=traci.junction.getIDList()
+    print("IDs of junctions:", IDsOfJunctions)
 
-traci.close()
+    traci.close()
+
+
+def get_options():
+    optParser = optparse.OptionParser()
+    optParser.add_option("--nogui", action="store_true",
+                         default=False, help="run the commandline version of sumo")
+    options, args = optParser.parse_args()
+    return options
+
+
+# this is the main entry point of this script
+if __name__ == "__main__":
+    options = get_options()
+
+    # this script has been called from the command line. It will start sumo as a
+    # server, then connect and run
+    if options.nogui:
+        sumoBinary = checkBinary('sumo')
+    else:
+        sumoBinary = checkBinary('sumo-gui')
+
+    # first, generate the route file for this simulation
+    # generate_routefile()
+
+    # this is the normal way of using traci. sumo is started as a
+    # subprocess and then the python script connects and runs
+    traci.start([sumoBinary, "-c", "cenary.sumocfg",
+                             "--tripinfo-output", "tripinfo.xml"])
+    run()
