666ec1eab6005a124c4ce5b80900af019df88c90
holynacll <alexandre.cury@xtesting.com.br>
2024-03-17 00:33:16 -0300

    implementado caso base, melhorias mapeadas

--- simple-grid/README.md
+++ simple-grid/README.md
@@ -1,7 +1,7 @@
 Docs:
   - https://sumo.dlr.de/pydoc/
 
-Cenário: 
+Cenário:
   - network grid, com carros, semáforos e Ponto A e Ponto B
     - distancia eucl, manh e harvesine
   - ocorrência de acidente (parada de carro/congestionamento no local/fechamento da via)
@@ -9,23 +9,33 @@ Cenário:
   - criação do algoritmo de liberação de sinal verde
   - comparação entre cenários/features/níveis/...
   - extensão da linha de pesquisa para n caminhos...
-    
 
-0 - $SUMO_HOME = "C:\Users\Alexandre Cury\miniconda3\envs\sumo-env\Lib\site-packages\sumo"
-0 - export SUMO_HOME="/home/acll/miniconda3/envs/sumo-env/lib/python3.11/site-packages/sumo"
+  TODO
+  - article: Analysis and modelling of road traffic using SUMO tooptimize the arrival time of emergency vehicles
+   - melhorar cenario do acidente
+   - quando um VE se aproximar de um sinal, tornar ele verde (já está mantendo verde, precisa virar verde, se tiver vermelho ou amarelo)
+     - article: Modelling green waves for emergency vehicles usingconnected traffic data
+   - priorizacao do VE
+   - veiculos comuns priorizar veiculos de emergencia (dar passagem)
+   - limitar e parametrizar tempo de simulação
+   - criar hospitais parametrizavel
+   - parametrizar as váriaveis do cenário
+   - configurar parada do veículo de emergência no local do acidente
+   - customizar/alternar algoritmos de roteamento
+   - ajustar retorno dos veiculos na mesma via
+   - ajustar controle de sinais "gggyyyrrr"
 
-1 - netgenerate --grid --grid.number=4 --grid.length=150 --default.lanenumber 2 --default-junction-type traffic_light --output-file=cenary.net.xml 
+0.1 - $SUMO_HOME = "C:\Users\Alexandre Cury\miniconda3\envs\sumo-env\Lib\site-packages\sumo"
+0.2 - export SUMO_HOME="/home/acll/miniconda3/envs/sumo-env/lib/python3.11/site-packages/sumo"
 
-1 - netgenerate --grid --grid.x-number=4 --grid.y-number=2 --grid.y-length=40 --grid.x-length=100 --default.lanenumber 1 --default-junction-type traffic_light --output-file=b_cenary.net.xml 
+1 - netgenerate --grid --grid.number=4 --grid.length=150 --default.lanenumber 2 --default-junction-type traffic_light --output-file=road.net.xml
 
-2 - python $SUMO_HOME/tools/randomTrips.py -n cenary.net.xml -o odtrips.car.xml -r cenary.car.rou.xml --prefix car --period 1 -p 1 --trip-attributes='color=\"0,0,1\" accel=\"0.8\" decel=\"4.5\" sigma=\"0.5\" length=\"5\" minGap=\"2.5\" maxSpeed=\"16.67\" guiShape=\"passenger\"'
+2 - python $SUMO_HOME/tools/randomTrips.py -n road.net.xml -r route.rou.xml --seed 42 --validate --fringe-factor 1000
 
-3 - python $SUMO_HOME/tools/randomTrips.py -n cenary.net.xml -o odtrips.emergency.xml -r cenary.emergency.rou.xml --prefix emergency --period 1000 -p 150 --vehicle-class emergency --trip-attributes='color=\"1,0,0\"'
+3 - add below code on first line into <routes> of the file route.rou.xml
+  "<vType id="emergency_emergency" vClass="emergency" color="red"/>"
 
-4 - python $SUMO_HOME/tools/generateParkingAreas.py -n cenary.net.xml -o parkingareas.add.xml --space-length 10 --probability 0.1
-
-last - sumo-gui cenary.sumocfg
-
+4 - python main.py
 
 bibliografia:
 Analysis and Modelling of Road Traffic Using SUMO to Optimize the Arrival Time of Emergency Vehicles - https://www.tib-op.org/ojs/index.php/scp/article/view/225/428 - https://www.youtube.com/watch?v=GlPf7TmuI9E (verificar o algoritmo)
@@ -51,3 +61,23 @@ https://www.researchgate.net/publication/37454736_TraCI_An_Interface_for_Couplin
 
 bugs:
 cars not stopping on red light: https://sourceforge.net/p/sumo/mailman/message/30703209/
+
+
+
+
+ 1935  python $SUMO_HOME/tools/randomTrips.py -n road.net.xml
+ 1936  python $SUMO_HOME/tools/assign/duaIterate.py -n road.net.xml -t trips.trips.xml -l 10
+ 1941  python $SUMO_HOME/tools/assign/one-shot.py -f 200 -n road.net.xml -t trips.trips.xml
+ 1942  python $SUMO_HOME/tools/randomTrips.py -n road.net.xml --seed 42 --validate
+ 1943  python $SUMO_HOME/tools/assign/one-shot.py -f 200 -n road.net.xml -t trips.trips.xml
+ 1948  python $SUMO_HOME/tools/randomTrips.py -n road.net.xml --seed 42 --validate
+ 1949  python $SUMO_HOME/tools/assign/one-shot.py -f 200 -n road.net.xml -t trips.trips.xml
+ 1957  netgenerate --grid --grid.number=2 --grid.length=150 --default.lanenumber 2 --default-junction-type traffic_light --output-file=road.net.xml
+ 1959  netgenerate --grid --grid.number=3 --grid.length=150 --default.lanenumber 2 --default-junction-type traffic_light --output-file=road.net.xml
+ 1968  netedit road.net.xml 
+ 1970  python $SUMO_HOME/tools/randomTrips.py -n road.net.xml --seed 42 --validate --fringe-factor 1000
+ 1971  netgenerate --grid --grid.number=3 --grid.length=150 --default.lanenumber 2 --default-junction-type traffic_light --output-file=road.net.xml
+ 1972  python $SUMO_HOME/tools/assign/one-shot.py -f 10 -n road.net.xml -t trips.trips.xml
+
+
+
--- simple-grid/main.py
+++ simple-grid/main.py
@@ -3,6 +3,7 @@ import sys
 import time
 import optparse
 import random
+import traceback
 os.environ['SUMO_HOME'] = "/home/acll/miniconda3/envs/sumo-env/lib/python3.11/site-packages/sumo"
 
 if 'SUMO_HOME' in os.environ:
@@ -11,15 +12,12 @@ if 'SUMO_HOME' in os.environ:
 else:
     sys.exit("please declare environment variable 'SUMO_HOME'")
 
-# sumoBinary = os.path.join(tools, "sumo-gui")
-# sumo_gui = 'sumo-gui'
-# file = "cenary.sumocfg"
-# sumoCmd = [sumo_gui, "-c", file]
-
 import traci
 import traci.constants as tc
 from sumolib import checkBinary  # noqa
 
+monitor_emergency_vehicles_list = []
+hospital_pos = 'A1B1'
 
 def check_tls(tls_id):
     print("checking tls_id: ", tls_id)
@@ -41,6 +39,26 @@ def check_vehicle(vehicle_id):
     print("Distance ", vehicle_id, ": ", traci.vehicle.getDistance(vehicle_id), " m")
 
 
+def get_network_parameters():
+    IDsOfEdges=traci.edge.getIDList()
+    print("IDs of the edges:", IDsOfEdges)
+    IDsOfJunctions=traci.junction.getIDList()
+    print("IDs of junctions:", IDsOfJunctions)
+
+def get_statistics_from_timeloss_and_halting(junction_id):
+    scResults = traci.junction.getContextSubscriptionResults(junction_id)
+    stepLength = traci.simulation.getDeltaT()
+    halting = 0
+    if scResults:
+        relSpeeds = [d[tc.VAR_SPEED] / d[tc.VAR_ALLOWED_SPEED] for d in scResults.values()]
+        # compute values corresponding to summary-output
+        running = len(relSpeeds)
+        halting = len([1 for d in scResults.values() if d[tc.VAR_SPEED] < 0.1])
+        meanSpeedRelative = sum(relSpeeds) / running
+        timeLoss = (1 - meanSpeedRelative) * running * stepLength
+    print(traci.simulation.getTime(), timeLoss, halting)
+
+
 def shouldContinueSim():
     """Checks that the simulation should continue running.
     Returns:
@@ -50,46 +68,109 @@ def shouldContinueSim():
     return True if numVehicles > 0 else False
 
 
-def improve_traffic_on_accident(emergency_route_id):
-    vehicles = traci.vehicle.getIDList()
-    for veh_id in vehicles:
-        type_id = traci.vehicle.getTypeID(veh_id)
-        if type_id != 'emergency_emergency':
-            next_roads_list = traci.vehicle.getRoute(veh_id)
-            # print('next_roads_lists:', next_roads_list, 'emergency_route_id:', emergency_route_id)
-            if emergency_route_id in next_roads_list:
-                # check_vehicle(veh_id)
-                print(f"emergency_route_id: {emergency_route_id} and traveltime: {traci.edge.getTraveltime(emergency_route_id)}")
-                print(f"some_route_id: {next_roads_list[0]} and traveltime: {traci.edge.getTraveltime(next_roads_list[0])}")
-                # traci.vehicle.setAdaptedTraveltime(veh_id, emergency_route_id, float('inf'))
-                # traci.edge.adaptTraveltime(emergency_route_id,traci.edge.getTraveltime(emergency_route_id))
-                # traci.vehicle.rerouteTraveltime(veh_id)
-                traci.vehicle.setColor(veh_id, color=(0, 255, 0))
+def improve_traffic_on_accidented_road(vehicles):
+    for monitor_emergency_vehicle in monitor_emergency_vehicles_list:
+        emergency_route_id = monitor_emergency_vehicle['emergency_route_id']
+        for veh_id in vehicles:
+            type_id = traci.vehicle.getTypeID(veh_id)
+            if type_id != 'emergency_emergency':
+                next_roads_list = traci.vehicle.getRoute(veh_id)
+                if emergency_route_id in next_roads_list:
+                    traci.edge.adaptTraveltime(emergency_route_id,traci.edge.getTraveltime(emergency_route_id))
+                    traci.vehicle.rerouteTraveltime(veh_id)
+                    if next_roads_list[-1] == emergency_route_id:
+                        traci.vehicle.setColor(veh_id, color=(0, 100, 100))
+                    else:
+                        traci.vehicle.setColor(veh_id, color=(0, 255, 0))
 
 
-def monitor_emergency_vehicles(monitor_emergency_vehicles_list: list):
+def speed_road_recovery(emergency_route_id):
+    MAX_SPEED_ROAD_RECOVERED = 50
+    can_increase_max_speed = True
+    for monitor_road in  monitor_emergency_vehicles_list:
+        # se existe algum acidente em andamento
+        if monitor_road['emergency_route_id'] == emergency_route_id:
+            can_increase_max_speed = False
+    if can_increase_max_speed:
+        traci.edge.setMaxSpeed(emergency_route_id, MAX_SPEED_ROAD_RECOVERED)
+        print("max speed road recovered")
+
+
+def monitor_emergency_vehicles():
+    MIN_ARRIVAL_DISTANCE_EMERGENCY_VEHICLE_AT_THE_ACCIDENT = 1.0
     for key, monitor_emergency_vehicle in enumerate(monitor_emergency_vehicles_list):
         veh_accidented_id = monitor_emergency_vehicle['veh_accidented_id']
         veh_emergency_id = monitor_emergency_vehicle['veh_emergency_id']
         emergency_route_id = monitor_emergency_vehicle['emergency_route_id']
         arrival_pos = monitor_emergency_vehicle['arrival_pos']
         hospital_pos = monitor_emergency_vehicle['hospital_pos']
-        # print('monitoring', monitor_emergency_vehicles_list)
-        improve_traffic_on_accident(emergency_route_id)
         actual_road = traci.vehicle.getRoadID(veh_emergency_id)
-        # print('actual_road:', actual_road, 'emergency_route_id:', emergency_route_id)
         if actual_road == emergency_route_id:
             distance = traci.vehicle.getDrivingDistance(veh_emergency_id, actual_road, arrival_pos)
-            if  distance < 1.0:
-                # new_emergency_route_id_back_to_hospital = f"rou_emergency_{traci.simulation.getTime()}"
-                # traci.route.add(routeID=new_emergency_route_id_back_to_hospital, edges=[actual_road, hospital_pos])
+            if  distance < MIN_ARRIVAL_DISTANCE_EMERGENCY_VEHICLE_AT_THE_ACCIDENT:
+                # stop emergency vehicle for some duration
                 traci.vehicle.remove(veh_accidented_id)
-                traci.edge.setMaxSpeed(emergency_route_id, 50)
                 traci.vehicle.changeTarget(veh_emergency_id, hospital_pos)
                 monitor_emergency_vehicles_list.pop(key)
-                # stop vehicle for some duration
-                # rerouter emergency vehicle to hospital
-                # release vehicle accidented
+                speed_road_recovery(emergency_route_id)
+                
+
+
+def create_accident_and_call_emergency_vehicle(vehicles):
+    MAX_SPEED_ROAD_ACCIDENTED = 0.3
+    random_number = random.randrange(0, len(vehicles))
+    veh_accidented_id = vehicles[random_number]
+    for monitor_emergency_vehicle in monitor_emergency_vehicles_list:
+        if monitor_emergency_vehicle['veh_accidented_id'] == veh_accidented_id:
+            print('verify how to handle with that case')
+            return None
+    road_id_accidented = traci.vehicle.getRoadID(veh_accidented_id)
+    traci.edge.setMaxSpeed(road_id_accidented, MAX_SPEED_ROAD_ACCIDENTED)
+    # traci.vehicle.setSpeedMode(veh_accidented_id, 0)
+    traci.vehicle.setSpeed(veh_accidented_id, 0)
+    traci.vehicle.highlight(veh_accidented_id, (0, 0, 255, 255))
+    emergency_route_id = f"rou_emergency_{traci.simulation.getTime()}"
+    veh_emergency_id = f"veh_emergency_{traci.simulation.getTime()}"
+    arrival_pos = traci.vehicle.getLanePosition(veh_accidented_id)
+    traci.route.add(routeID=emergency_route_id, edges=[hospital_pos, road_id_accidented])
+    traci.vehicle.add(
+        vehID=veh_emergency_id,
+        routeID=emergency_route_id,
+        typeID="emergency_emergency",
+        depart='now',
+        departLane='best',
+        departPos='base',
+        departSpeed='0',
+        arrivalLane='current',
+        arrivalPos='max',
+        arrivalSpeed='current'
+    )
+    monitor_emergency_vehicles_list.append({
+        'veh_accidented_id': veh_accidented_id,
+        'veh_emergency_id':veh_emergency_id,
+        'emergency_route_id': road_id_accidented,
+        'arrival_pos': arrival_pos,
+        'hospital_pos': hospital_pos,
+    })
+
+
+def improve_traffic_for_emergency_vehicle(vehicles):
+    for veh_id in vehicles:
+        type_id = traci.vehicle.getTypeID(veh_id)
+        if type_id == 'emergency_emergency':
+            """
+            Return list of upcoming traffic lights [(tlsID, tlsIndex, distance, state), ...]
+            cada tls está em ordem crescente de distância 
+            The lanePosition is the driving distance from the start of the edge (road) in meters
+            """
+            next_tls_set = traci.vehicle.getNextTLS(veh_id)
+            for tls in next_tls_set:
+                tls_id = tls[0]
+                vehicle_distance_to_tls = tls[2]
+                tls_state = tls[3]
+                if vehicle_distance_to_tls < 40:
+                    if tls_state in ('g', 'G'):
+                        traci.trafficlight.setPhaseDuration(tls_id, 5)
 
 
 def run():
@@ -99,133 +180,23 @@ def run():
         junctionID, tc.CMD_GET_VEHICLE_VARIABLE, 1000000,
         [tc.VAR_SPEED, tc.VAR_ALLOWED_SPEED]
     )
-    allowed_vehicles_type_on_emergency = ['emergency']
-    monitor_emergency_vehicles_list = []
-    hospital_pos = 'A1B1'
-    while shouldContinueSim():
-        traci.simulationStep()
-        monitor_emergency_vehicles(monitor_emergency_vehicles_list)
-        vehicles = traci.vehicle.getIDList()
-        # em um dado momento, acidente ocorre
-        if traci.simulation.getTime()%250 == 0:
-            random_number = random.randrange(0, len(vehicles))
-            veh_accidented_id = vehicles[random_number]
-            road_id_accidented = traci.vehicle.getRoadID(veh_accidented_id)
-            # create accident
-            # Regard safe speed
-            traci.edge.setMaxSpeed(road_id_accidented, 0.3)
-            traci.vehicle.setSpeedMode(veh_accidented_id, 0)
-            traci.vehicle.setSpeed(veh_accidented_id, 0)
-            traci.vehicle.highlight(veh_accidented_id, (0, 0, 255, 255))
-            emergency_route_id = f"rou_emergency_{traci.simulation.getTime()}"
-            veh_emergency_id = f"veh_emergency_{traci.simulation.getTime()}"
-            arrival_pos = traci.vehicle.getLanePosition(veh_accidented_id)
-            traci.route.add(routeID=emergency_route_id, edges=[hospital_pos, road_id_accidented])
-            traci.vehicle.add(
-                vehID=veh_emergency_id,
-                routeID=emergency_route_id,
-                typeID="emergency_emergency",
-                depart='now',
-                departLane='best',
-                departPos='base',
-                departSpeed='0',
-                arrivalLane='current',
-                arrivalPos='max',
-                arrivalSpeed='current'
-            )
-            monitor_emergency_vehicles_list.append({
-                'veh_accidented_id': veh_accidented_id,
-                'veh_emergency_id':veh_emergency_id,
-                'emergency_route_id': road_id_accidented,
-                'arrival_pos': arrival_pos,
-                'hospital_pos': hospital_pos,
-            })
-            # broadcast message to vanets? how do i announce an accident?
-            # traci.edge.setAllowed(road_id, allowed_vehicles_type_on_emergency)
-            # x, y = traci.vehicle.getPosition(some_vehicle_id)
-            # traci.poi.add("140907752#1", x, y, color=(255, 0, 0), type="shop")
 
-        # if traci.simulation.getTime()%500==0 and accidents_num == 0:
-        #     accidents_num -= 1
-        #     traci.vehicle.setSpeedMode(some_vehicle_id, 1)
-        #     traci.vehicle.setSpeed(some_vehicle_id, 40.0)
-        #     traci.edge.setMaxSpeed(road_id, 50.0)
-        for vehicle_id in vehicles:
-            type_id = traci.vehicle.getTypeID(vehicle_id)
-            if type_id == 'emergency_emergency':
-                """
-                Return list of upcoming traffic lights [(tlsID, tlsIndex, distance, state), ...]
-                cada tls está em ordem crescente de distância 
-                The lanePosition is the driving distance from the start of the edge (road) in meters
-                """
-                next_tls_set = traci.vehicle.getNextTLS(vehicle_id)
-                # print("next tls for vehicle emergency:", vehicle_id, " is ", next_tls_set)
-                for tls in next_tls_set:
-                    # get tls
-                    tls_id = tls[0]
-                    tls_index = tls[1]
-                    vehicle_distance_to_tls = tls[2]
-                    tls_state = tls[3]
-                    if vehicle_distance_to_tls < 40:
-                        if tls_state in ('g', 'G'):
-                            traci.trafficlight.setPhaseDuration(tls_id, 5)
-                            # traci.trafficlight.setPhase(tls_id, 0)
-                    # check_tls(tls_id)
-                    # tls_phases = traci.trafficlight.getRedYellowGreenState(tls_id)
-                    # print(tls_phases)
-                    # for index, phase in enumerate(tls_phases):
-                    #     if phase in ('g', 'G'):
-                    #         # traci.trafficlight.setPhase(tls_id, int(index))
-                    #         traci.trafficlight.setPhaseDuration(tls_id, 10)
-                    #         break
-                    
-                    # vehicle_speed = traci.vehicle.getSpeed(vehicle_id)
-                    # time_to_tls = tls_distance/vehicle_speed
-        # tls_id_list = traci.trafficlight.getIDList()
-        # for tls_id in tls_id_list:
-        #     links_controlleds = traci.trafficlight.getControlledLinks(tls_id)
-        #     print('links controllers: ', links_controlleds)
-        #     for links_set_list in links_controlleds:
-        #         for link_set in links_set_list:
-        #             for link in link_set:
-        #                 print('link: ', link)
-        #                 vehiclesblock = traci.trafficlight.getBlockingVehicles(tls_id, str(link))
-        #                 print('cars blocked', vehiclesblock)
-        # print(edges)
-        # for edge_id in edges:
-        #     if traci.inductionloop.getLastStepVehicleNumber(edge_id) > 0:
-        #         print(f"carro passou pela edge {edge_id}")
-        
-        # if step%10==0:
-        #     for i in range(0, len(vehicles)):
-        #         print(vehicles[i])
-        #         traci.vehicle.setSpeed(vehicles[i], 15)  
-        #         traci.gui.toggleSelection(vehicles[i])
-                
-                
-        #     print("Speed ", vehicles[i], ": ", traci.vehicle.getSpeed(vehicles[i]), " m/s")
-        #     print("CO2Emission ", vehicles[i], ": ", traci.vehicle.getCO2Emission(vehicles[i]), " mg/s")
-        #     print("EdgeID of veh ", vehicles[i], ": ", traci.vehicle.getRoadID(vehicles[i]))
-        #     print("Distance ", vehicles[i], ": ", traci.vehicle.getDistance(vehicles[i]), " m")
-        # scResults = traci.junction.getContextSubscriptionResults(junctionID)
-        # halting = 0
-        # if scResults:
-        #     relSpeeds = [d[tc.VAR_SPEED] / d[tc.VAR_ALLOWED_SPEED] for d in scResults.values()]
-        #     # compute values corresponding to summary-output
-        #     running = len(relSpeeds)
-        #     halting = len([1 for d in scResults.values() if d[tc.VAR_SPEED] < 0.1])
-        #     meanSpeedRelative = sum(relSpeeds) / running
-        #     timeLoss = (1 - meanSpeedRelative) * running * stepLength
-        # print(traci.simulation.getTime(), timeLoss, halting)
-        step += 1
+    try:
+        while shouldContinueSim():
+            traci.simulationStep()
+            monitor_emergency_vehicles()
+            vehicles = traci.vehicle.getIDList()
+            if traci.simulation.getTime()%250 == 0:
+                create_accident_and_call_emergency_vehicle(vehicles)
+            improve_traffic_on_accidented_road(vehicles) # reroute vehicles to avoid of the aciddented road
+            improve_traffic_for_emergency_vehicle(vehicles) # green wave
+            # get_statistics_from_timeloss_and_halting(junctionID)    
+            step += 1
+        # get_network_parameters()
+        traci.close()
+    except Exception as e:
+        print(traceback.format_exc())
 
-    #get network parameters
-    # IDsOfEdges=traci.edge.getIDList()
-    # print("IDs of the edges:", IDsOfEdges)
-    # IDsOfJunctions=traci.junction.getIDList()
-    # print("IDs of junctions:", IDsOfJunctions)
-
-    traci.close()
 
 
 def get_options():
@@ -247,11 +218,8 @@ if __name__ == "__main__":
     else:
         sumoBinary = checkBinary('sumo-gui')
 
-    # first, generate the route file for this simulation
-    # generate_routefile()
-
     # this is the normal way of using traci. sumo is started as a
     # subprocess and then the python script connects and runs
-    traci.start([sumoBinary, "-c", "one_shot_10.sumocfg",
+    traci.start([sumoBinary, "-c", "config.sumocfg",
                              "--tripinfo-output", "tripinfo.xml"])
     run()
